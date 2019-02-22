8ebf2754b006070bc13ba95c24613332ac1b4f70
Siddarth <sidr97@gmail.com>
2019-02-22 00:45:01 -0800

    Fixed Samtools Sort Memory Issue
    Used the Samtools sort function to sort the BAM file initially. Due to this, the laptop should avoid running out of memory and no longer needs to be run on the server.
    
    The cellsorted BAM file will then be accepted and incorporated into the velocyto program.

--- RNA_Velocity_10x_Pipeline.py
+++ RNA_Velocity_10x_Pipeline.py
@@ -7,5 +7,10 @@ import os
 #Import Files for the loom contruction from the GUI
 Output_Folder, Gene_File = Get_Files()
 
+#Run Samtools Sort initiall allow for the tool to run on a laptop by presorting the BAM file. 
+os.system("samtools sort -l 0 -O BAM  %s/outs/possorted_genome_bam.bam -o %s/outs/cellsorted_possorted_genome_bam.bam" %(Output_Folder,Output_Folder))
+# NOTE Due to the possible issues associated with the large file created by the Samtools,
+# NOTE it might be worth allowing people to adjust varible in the sort function in the future.
+
 #Run Velocity Command
 os.system("velocyto run10x %s %s" % (Output_Folder, Gene_File,))
